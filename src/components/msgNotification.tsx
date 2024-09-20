d5431577722d77542ab7510b1cb16de762cdeb3a
Sara Romao <sara-romao@live.com>
2024-09-19 22:15:20 -0300

    feat: add header notification e count total

--- src/components/msgNotification.tsx
+++ src/components/msgNotification.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Notification } from "../types/notification";
 import './styles.css';
 
@@ -7,26 +8,54 @@ type NotificationProps ={
 
 
 export const NotificationComp = ({ notifications }:NotificationProps) => {
+   
+  const [totalNotification, setTotalNotification] = useState(0);
+
+  useEffect(() => {
+    const total = notifications.reduce((acc, item) => {
+      return item.status === 'unseen' ? acc + 1 : acc;
+    }, 0);
+    
+    setTotalNotification(total);
+  }, [notifications]);
 
 
   return (
     <div className="notification-container">
 
+      <div className="header-notification">
+        <div className="count-notification-container">
+          <h2>Notifications</h2>
+          <p className="count-notification">{totalNotification}</p>
+        </div>
+
+        <button className="btn-mark-all">Mark all as read</button>
+      </div>
+
        {notifications.map(item => (
-          <div className="msg-container">
+          <div className="msg-container" key={item.id}>
             
             <div  className={`msgTitle ${item.status === 'unseen' ? 'unseen' : ''}`} >
                 <img className="imgprofile" width={45} height={45} src="https://images.unsplash.com/photo-1726251654985-b415579cd295?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D" alt="" />
                 
-                <p><span className="name">Rizky Hasanuddi </span> <span>sent you a private message</span></p>
+                <p><span className="name">{item.name} </span> <span>{item.reason}</span> {item.groupName &&(<span>{item.groupName}</span>)}  </p>
                 {item.status === 'unseen' ? (<span className="unseen-detalhe"></span>) : ''}
-              </div>
+            </div>
+             
+             {item.text && (
+                <div className="txtMensagem">
+                  <p>
+                    Hello, thanks for setting up the Chess Club. I’ve been a member for a few weeks now and I’m already having lots of fun and improving my game.
+                  </p>
+                </div>
 
-              <div className="txtMensagem">
-                <p>
-                  Hello, thanks for setting up the Chess Club. I’ve been a member for a few weeks now and I’m already having lots of fun and improving my game.
-                </p>
+
+             )  }
+
+              <div>
+                <img src="" alt="" />
               </div>
+              
 
           </div>
        ))}
